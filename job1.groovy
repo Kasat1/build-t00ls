e5e8de4feed27e8898ccb9ec2f331ec459c4de78
Kasat1 <99502652+Kasat1@users.noreply.github.com>
2022-03-22 11:24:49 +0300

    Create job1.groovy

---
+++ job1.groovy
@@ -0,0 +1,119 @@
+def gitUrl = 'https://github.com/Kasat1/build-t00ls/tree/main'
+
+job('MNTLAB-akasatau-main-build-job') {
+    description 'This is main-build-job'
+    parameters {
+    	stringParam('BRANCH_NAME')
+        activeChoiceParam('JOB_TO_EXEC'){
+        	choiceType('CHECKBOX')
+            groovyScript{
+            	script('["MNTLAB-akasatau-child1-build-job","MNTLAB-akasatau-child2-build-job","MNTLAB-akasatau-child3-build-job","MNTLAB-akasatau-child4-build-job"]')
+            fallbackScript()
+            }
+        }
+    }
+      concurrentBuild()
+  		steps {
+    		triggerBuilder {
+      			configs {
+        			blockableBuildTriggerConfig {
+         				 projects('$JOB_TO_EXEC')
+          configs {
+            predefinedBuildParameters {
+              properties('BRANCH_NAME=$BRANCH_NAME')
+              textParamValueOnNewLine(false)
+            }
+          } 
+        }
+      }
+    } 
+  }
+}
+
+job("MNTLAB-akasatau-child1-build-job"){
+  parameters {
+    activeChoiceParam('BRANCH_NAME') {
+        choiceType('SINGLE_SELECT')
+    }
+  }
+    scm {
+    git { gitUrl
+      }
+      branch '*/akasatau'
+    }
+    steps{
+    	maven('clean install')
+    }  
+  steps {
+    shell('''
+cd home-task/target/
+tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
+''')
+  }  
+}
+
+job("MNTLAB-akasatau-child2-build-job"){
+  parameters {
+    activeChoiceParam('BRANCH_NAME') {
+        choiceType('SINGLE_SELECT')
+    }
+  }
+    scm {
+    git { gitUrl
+      }
+      branch '*/akasatau'
+    }
+    steps{
+    	maven('clean install')
+    }  
+  steps {
+    shell('''
+cd home-task/target/
+tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
+''')
+  }  
+}
+
+job("MNTLAB-akasatau-child3-build-job"){
+  parameters {
+    activeChoiceParam('BRANCH_NAME') {
+        choiceType('SINGLE_SELECT')
+    }
+  }
+    scm {
+    git { gitUrl
+      }
+      branch '*/akasatau'
+    }
+    steps{
+    	maven('clean install')
+    }  
+  steps {
+    shell('''
+cd home-task/target/
+tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
+''')
+  }  
+}
+
+job("MNTLAB-akasatau-child4-build-job"){
+  parameters {
+    activeChoiceParam('BRANCH_NAME') {
+        choiceType('SINGLE_SELECT')
+    }
+  }
+    scm {
+    git { gitUrl
+      }
+      branch '*/akasatau'
+    }
+    steps{
+    	maven('clean install')
+    }  
+  steps {
+    shell('''
+cd home-task/target/
+tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
+''')
+  }  
+}       
