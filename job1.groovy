d1bf3345e89fc41da0c6cc93cf33451baada9070
Kasat1 <99502652+Kasat1@users.noreply.github.com>
2022-03-25 09:15:25 +0300

    Update job1.groovy

--- job1.groovy
+++ job1.groovy
@@ -1,74 +1,87 @@
 def gitUrl = 'https://github.com/Kasat1/build-t00ls/tree/main'
-
+    
 job('MNTLAB-akasatau-main-build-job') {
-    description 'This is main-build-job'
-    parameters {
-    	stringParam('BRANCH_NAME')
-        activeChoiceParam('JOB_TO_EXEC'){
-        	choiceType('CHECKBOX')
-            groovyScript{
-            	script('["MNTLAB-akasatau-child1-build-job","MNTLAB-akasatau-child2-build-job","MNTLAB-akasatau-child3-build-job","MNTLAB-akasatau-child4-build-job"]')
-            fallbackScript()
-            }
-        }
-    }
-}
+  description 'This is Main job'
 
-job("MNTLAB-akasatau-child1-build-job"){
   parameters {
-    activeChoiceParam('BRANCH_NAME') {
-        choiceType('SINGLE_SELECT')
+    gitParam('BRANCH_NAME') {
+      type 'BRANCH'
+      defaultValue 'origin/akasatau'
+    }
+    activeChoiceReactiveParam('Job_name') {
+           description('Choice from multiple parameters')
+           choiceType('CHECKBOX')
+           groovyScript {
+               script('return ["MNTLAB-akasatau-child1-build-job", "MNTLAB-akasatau-child2-build-job", "MNTLAB-akasatau-child3-build-job", "MNTLAB-akasatau-child4-build-job"]')
+           }
     }
   }
-    scm {
-    git { gitUrl 
-         branch '$BRANCH_NAME'
-        }
-    steps{
-    	maven('clean install')
-    }  
-  steps {
-    shell('''
-cd home-task/target/
-tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
-''')
-  }  
-}
-
-job("MNTLAB-TEST"){
-  parameters {
-    activeChoiceParam('BRANCH_NAME') {
-      description('Branch name')
-        choiceType('SINGLE_SELECT')
-        groovyScript {
-        script('''
-def command = "git ls-remote -h $gitURL"
-return command
-''')
-        fallbackScript()
+  
+  scm {
+    git {
+      remote {
+          url {gitUrl}
       }
+      branch '$BRANCH_NAME'
+    }
+  }
+  steps {
+       downstreamParameterized {
+               trigger('$Job_name') {
+                 block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                 }
+                 parameters {
+                     currentBuild()
+                 }
+           }
+       }
+   }
+}
+def JOBS = ["MNTLAB-akasatau-child1-build-job", "MNTLAB-akasatau-child2-build-job", "MNTLAB-akasatau-child3-build-job", "MNTLAB-akasatau-child4-build-job"]
+for(job in JOBS) {
+
+
+mavenJob(job) {
+  description 'Child jobs'
+  parameters {
+    gitParam('BRANCH_NAME') {
+      description 'Jenkins Task6 child jobs'
+      type 'BRANCH'
+    }
+    activeChoiceReactiveParam('Job_name') {
+           description('Multiple choice')
+           choiceType('CHECKBOX')
+           groovyScript {
+               script('return ["MNTLAB-akasatau-child1-build-job", "MNTLAB-akasatau-child2-build-job", "MNTLAB-akasatau-child3-build-job", "MNTLAB-akasatau-child4-build-job"]')
+           }
     }
   }  
-  steps{
-    	maven('clean install')
-    }  
-  steps {
-    shell('''
-cd home-task/target/
-tar cvf "$BRANCH_NAME_dsl_script.tar.gz" *.jar
-''')
+
+  scm {
+    git {
+      remote {
+          url {gitUrl}
+      }
+      branch '$BRANCH_NAME'
+    }
+  }
+
+  triggers {
+    scm 'H/60 * * * *'
+  }
+
+  rootPOM 'home-task/pom.xml'
+  goals 'clean install'
+  postBuildSteps {
+        shell('java -cp home-task/target/BuildArtifact-1.0.jar com.test.Project >> home-task/target/output.log')
+        shell('tar -cvf "$(echo $BRANCH_NAME | cut -d "/" -f 2)_dsl_script.tar.gz" home-task/target/*.jar home-task/target/output.log')
+    }
+  
+  publishers {
+        archiveArtifacts('*.tar.gz')
+    }
 }
 }
-}
-// def gitURL = "Kasat1/build-t00ls (github.com)"
-// def command = "git ls-remote -h \$gitURL"
-// def proc = command.execute()
-// proc.waitFor()
-// if ( proc.exitValue() != 0 ) {
-//   println "Error, \${proc.err.text}"
-//   System.exit(-1)
-// }
-// def branches = proc.in.text.readLines().collect {
-//   it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
-// }
-// return branches
